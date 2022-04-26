251de4f0f423f95a2115f3884af05bf7478a1453
hung890202 <bir0413520@gmail.com>
2022-04-26 16:36:35 +0800

    4/26

--- pages/Coinspage/CoinsList.js
+++ pages/Coinspage/CoinsList.js
@@ -1,8 +1,28 @@
 import CoinsItem from "./CoinsItem";
+import { 
+  StyledTital,
+  Coin,
+  CoinData,
+  Price,
+  Volume,
+  Change,
+  Mkt
+
+} from "./CoinsListStyles"
 
 export default function CoinsList({ filteredCoins }) {
   return (
     <>
+    <StyledTital>
+     
+      <Coin>Coin</Coin>
+      <CoinData>
+      <Price>Price</Price>
+      <Volume>24h Volume</Volume>
+      <Change>24h</Change>
+      <Mkt>Mkt Cap</Mkt>
+      </CoinData>
+      </StyledTital>
       {filteredCoins.map((coin) => {
         return (
           <CoinsItem
